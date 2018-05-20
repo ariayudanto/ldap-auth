55fdba19a77092850432b4d7d7cdc45992e43b31
Aria Yudanto <aria.yudanto@gmail.com>
2018-05-20 08:32:54 +0700

    improve code

--- src/main/java/com/aria/ldap/auth/Main.java
+++ src/main/java/com/aria/ldap/auth/Main.java
@@ -1,9 +1,8 @@
 package com.aria.ldap.auth;
 
 import javax.naming.Context;
-import javax.naming.NamingEnumeration;
-import javax.naming.NamingException;
-import javax.naming.directory.*;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
 import java.util.Hashtable;
 
 public class Main {
@@ -12,65 +11,41 @@ public class Main {
 
         // ldap config
         String url = "ldap://ldap.forumsys.com:389";
-
-        // service/manager credential --> use for binding LDAP
         String base = "dc=example,dc=com";
-        String managerUserDn = "cn=read-only-admin,dc=example,dc=com";
-        String managerPassword = "password";
-
-        // user credential
         String userSearchFilter = "uid";
         String userSearchBase = "";
 
-        // username and password --> get from login form
+        // username and password get from login
         String username = "einstein";
         String password = "password";
 
         boolean isUserValid = false;
 
+        // populate user search
+        StringBuilder userSearch = new StringBuilder();
+        userSearch.append(userSearchFilter + "=" + username);
+        userSearch.append(",");
+        if (userSearchBase.length() != 0) {
+            userSearch.append(userSearchBase);
+        } else {
+            userSearch.append(base);
+        }
+
         Hashtable env = new Hashtable();
         env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
         env.put(Context.PROVIDER_URL, url);
-        env.put(Context.SECURITY_PRINCIPAL, managerUserDn);
-        env.put(Context.SECURITY_CREDENTIALS, managerPassword);
+        env.put(Context.SECURITY_PRINCIPAL, userSearch.toString());
+        env.put(Context.SECURITY_CREDENTIALS, password);
 
         DirContext ctx = null;
         try {
             ctx = new InitialDirContext(env);
-            Attributes attributes = new BasicAttributes(true); // ignore case
-            attributes.put(userSearchFilter, username);
-
-            NamingEnumeration results = ctx.search(base, attributes);
-            while (results.hasMore()) {
-                SearchResult searchResult = (SearchResult) results.next();
-
-                System.out.println(searchResult.toString());
-
-                // attempt another authentication, now with the user
-                // populate user search
-                StringBuilder userSearch = new StringBuilder();
-                userSearch.append(userSearchFilter + "=" + username);
-                userSearch.append(",");
-                if (userSearchBase.length() != 0) {
-                    userSearch.append(userSearchBase);
-                } else {
-                    userSearch.append(base);
-                }
-
-                Hashtable authEnv = new Hashtable();
-                authEnv.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-                authEnv.put(Context.PROVIDER_URL, url);
-                authEnv.put(Context.SECURITY_PRINCIPAL, userSearch.toString());
-                authEnv.put(Context.SECURITY_CREDENTIALS, password);
-                new InitialDirContext(authEnv);
-
+            if (ctx != null)
                 isUserValid = true;
-
-            }
 
         } catch (Exception e) {
             isUserValid = false;
-            throw e;
+            e.printStackTrace();
         } finally {
             if (ctx != null)
                 ctx.close();
